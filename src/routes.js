2d4feb65229940d158769f1bce640ef64d60f682
FELIXJM <76449935+FELIXJM@users.noreply.github.com>
2024-02-25 00:15:05 -0300

    Incluido CADV em DADOSINS e DADOSINS000

--- src/routes.js
+++ src/routes.js
@@ -26,7 +26,7 @@ function ensureToken(req,res,next) {
 
 const routes = new Router();
 //
-routes.get('/', (req, res) => res.send('API MICROCAD V2.92 is running OK.'));
+routes.get('/', (req, res) => res.send('API MICROCAD V3.00 is running OK.'));
 //
 /** API App Mobile */
 routes.get('/api/search/:predicate',                   LicenseController.search);
