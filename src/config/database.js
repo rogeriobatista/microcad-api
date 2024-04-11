dabb9254041ff6770480e514c7bd611461f8d68d
Rogerio Batista <rogerio.batista@clevertech.biz>
2024-04-11 20:19:17 -0300

    Set database encode to utf-8

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ module.exports = {
    host: process.env.DB_HOST,
    port: 1433,
    loggging: false,
-  
+
    username: process.env.DB_USER,
    password: process.env.DB_PASS,
    database: process.env.DB_NAME,
@@ -15,8 +15,10 @@ module.exports = {
          encrypt: true,
          validateBulkLoadParameters: true,
        }
-   }, 
+   },
    define: {
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
       timestamps: false,
       underscored: true,
       underscoredAll: true,
